21a21ba5e0f659abca993c97b3bb2979a1d85934
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-26 23:45:06 +0900

    Create shared.js

---
+++ models/shared.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const dbSetting = require('../modules/dbInfo');
+
+const sequelize = new Sequelize('donote_beta', dbSetting.id, dbSetting.pw, {
+  host: dbSetting.host,
+  dialect: "mysql"
+});
+
+class SharedMetaIndex extends Sequelize.Model {}
+
+SharedMetaIndex.init({
+  note: {
+    type: Sequelize.STRING(65),
+    allowNull: false
+  },
+  id: {
+    type: Sequelize.STRING(32),
+    allowNull: false,
+    unique: 'compositeIndex'
+  },
+  isEditable: {
+    type: Sequelize.INTEGER(1)
+  }
+}, {
+  sequelize,
+  modelName: 'sharedMeta'
+});
+

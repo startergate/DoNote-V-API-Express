4f34b455b3312cba21127238aac097ec5c61055c
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-07 16:36:08 +0900

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 
+const Meta = require('./Meta');
 const Note = require('./Note');
+const Shared = require('./Shared');
+const User = require('./User');
 
 const config = require('modules/dbInfo');
 
@@ -9,5 +12,8 @@ const sequelize = new Sequelize('donote_beta', config.id, config.pw, {
   dialect: "mysql"
 });
 
+exports.meta = Meta(sequelize, Sequelize);
 exports.note = Note(sequelize, Sequelize);
+exports.shared = Shared(sequelize, Sequelize);
+exports.user = User(sequelize, Sequelize);
 

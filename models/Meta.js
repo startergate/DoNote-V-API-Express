6b554ca0cb4dc98f81761620cb05da53df0c3c3e
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-07 16:38:32 +0900

    Update Meta.js

--- models/Meta.js
+++ models/Meta.js
@@ -1,29 +1,17 @@
-const Sequelize = require('sequelize');
-
-const dbSetting = require('../modules/dbInfo');
-
-const sequelize = new Sequelize('donote_beta', dbSetting.id, dbSetting.pw, {
-  host: dbSetting.host,
-  dialect: "mysql"
-});
-
-class MetaIndex extends Sequelize.Model { }
-
-class MetaData extends Sequelize.Model { }
-
-MetaIndex.init({
-  metaid: {
-    type: Sequelize.CHAR(32),
-    allowNull: false
-  },
-  userid: {
-    type: Sequelize.CHAR(32),
-    allowNull: false
-  }
-}, {
-  sequelize,
-  modelName: 'metaIndex'
-});
+exports.MetaIndex = (sequelize, DataTypes) => {
+  sequelize.define({
+    metaid: {
+      type: Sequelize.CHAR(32),
+      allowNull: false
+    },
+    userid: {
+      type: Sequelize.CHAR(32),
+      allowNull: false
+    }
+  }, {
+    timestamps: true
+  });
+};
 
 MetaData.init({
   datatype: {

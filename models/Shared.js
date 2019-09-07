0a72721cc889b8fc9588ce9667b89b389d44e7b6
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-07 23:13:23 +0900

    SharedMetaInit 수정

--- models/Shared.js
+++ models/Shared.js
@@ -1,31 +1,22 @@
-const Sequelize = require('sequelize');
+exports.SharedMetaIndex = (sequelize, DataTypes) => {
+  return sequelize.define("SharedMetaData", {
+    note: {
+      type: DataTypes.STRING(65),
+      allowNull: false
+    },
+    id: {
+      type: DataTypes.STRING(32),
+      allowNull: false,
+      unique: 'compositeIndex'
+    },
+    isEditable: {
+      type: DataTypes.NUMBER(1)
+    }
+  }, {
+    timestamps: false
+  });
+};
 
-const dbSetting = require('../modules/dbInfo');
-
-const sequelize = new Sequelize('donote_beta', dbSetting.id, dbSetting.pw, {
-  host: dbSetting.host,
-  dialect: "mysql"
-});
-
-class SharedMetaIndex extends Sequelize.Model {}
-
-SharedMetaIndex.init({
-  note: {
-    type: Sequelize.STRING(65),
-    allowNull: false
-  },
-  id: {
-    type: Sequelize.STRING(32),
-    allowNull: false,
-    unique: 'compositeIndex'
-  },
-  isEditable: {
-    type: Sequelize.INTEGER(1)
-  }
-}, {
-  sequelize,
-  modelName: 'sharedMeta'
-});
 exports.SharedMetaData = (sequelize, DataTypes) => {
   return sequelize.define("SharedMetaData", {
     shareTable: {

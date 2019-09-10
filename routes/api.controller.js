b6d9692d391740326d85456314f1a12fa9f579a7
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-10 19:51:17 +0900

    Update api.controller.js

--- routes/api.controller.js
+++ routes/api.controller.js
@@ -27,3 +27,27 @@ exports.findNote = (req, res, next) => {
         res.sendStatus(500);
     });
 };
+
+exports.updateNote =  (req, res, next) => {
+    sid.loginAuth(req.headers.sid_clientid, req.params.sessid).then(info => {
+        if (info.is_vaild && info.is_succeed) {
+            res.status(500);
+            res.send({
+                type: 'error',
+
+                is_vaild: true,
+                is_succeed: false
+            });
+            return;
+        }
+        note.tableName = `notedb_${info.pid}`;
+        note.update({
+            name: req.body.name,
+            text: req.body.text,
+            edittime: new Date(Date.now()).toISOString()
+        }, {
+            where: {id: req.params.id},
+            returning: true
+        })
+    })
+};
